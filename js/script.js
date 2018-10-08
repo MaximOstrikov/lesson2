7e5f1e5087504527e0d4cb912d3d1d34bd15642a
Ostrikov Maxim <AspixOne@gmail.com>
2018-10-08 18:02:13 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -4,16 +4,16 @@ let elementId = 0;
 $(window).on('mousemove', function(e){
     if (options.started) {
         console.log(elementId);
-        $('.polpap').css("left", options.initialX + e.clientX - options.x + "px").css("top", options.initialY + e.clientY - options.y -80 + "px");
+        $('#'+elementId).parent().css("left", options.initialX + e.clientX - options.x + "px").css("top", options.initialY + e.clientY - options.y -80 + "px");
         if (e.clientX < options.x - options.initialX) {
-            $('.polpap').css("left", 0 + "px");
+            $('#'+elementId).parent().css("left", 0 + "px");
         }
         if (e.clientY < options.y - options.initialY + 80) {
-            $('.polpap').css("top", 0 + "px");
+            $('#'+elementId).parent().css("top", 0 + "px");
         }
         if (e.clientX > $(window).width() - 320 + options.x - options.initialX ) {
             let clntx = $(window).width();
-            $('.polpap').css("left", clntx - 320 + "px");
+            $('#'+elementId).parent().css("left", clntx - 320 + "px");
         }
     }
 });
@@ -21,8 +21,8 @@ $(document).on("mousedown", ".downpanel", function (e) {
     elementId = $(this).context.id;
     console.log(elementId);
     options.started = true;
-    options.initialX = $('.polpap').offset().left;
-    options.initialY = $('.polpap').offset().top;
+    options.initialX = $('#'+elementId).parent().offset().left;
+    options.initialY = $('#'+elementId).parent().offset().top;
     options.x = e.clientX;
     options.y = e.clientY;
     console.log(options);
